5d4619a40cb926535d6fef088973a7a91f544e52
emilylii1204 <emilylii.1204@gmail.com>
2023-09-26 21:23:11 -0400

    much change

--- main.js
+++ main.js
@@ -10,3 +10,11 @@ $(document).ready(function() {
 	});
 });
 
+
+$(document).ready(function() {
+  var $magic = $(".magic"),
+      magicWHalf = $magic.width() / 2;
+  $(document).on("mousemove", function(e) {
+    $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+  });
+});
